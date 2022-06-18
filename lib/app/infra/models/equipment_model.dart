276e9444bc30f0207c20744cf1f5919e5c87fb62
Carlos Batista <carlos@strawti.com>
2022-06-18 12:54:16 -0300

    EquipmentModel added

---
+++ lib/app/infra/models/equipment_model.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+class EquipmentModel {
+  final int id;
+  final int installationId;
+  final int equipmentCategoryId;
+  final int tensionLevelId;
+  final String name;
+  final String description;
+  final String make;
+  final String model;
+  final String createdAt;
+  final String updatedAt;
+
+  EquipmentModel({
+    required this.id,
+    required this.installationId,
+    required this.equipmentCategoryId,
+    required this.tensionLevelId,
+    required this.name,
+    required this.description,
+    required this.make,
+    required this.model,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'installation_id': installationId});
+    result.addAll({'equipment_category_id': equipmentCategoryId});
+    result.addAll({'tension_level_id': tensionLevelId});
+    result.addAll({'name': name});
+    result.addAll({'description': description});
+    result.addAll({'make': make});
+    result.addAll({'model': model});
+    result.addAll({'created_at': createdAt});
+    result.addAll({'updated_at': updatedAt});
+
+    return result;
+  }
+
+  factory EquipmentModel.fromMap(Map<String, dynamic> map) {
+    return EquipmentModel(
+      id: map['id'] ?? '',
+      installationId: map['installation_id'] ?? '',
+      equipmentCategoryId: map['equipment_category_id'] ?? '',
+      tensionLevelId: map['tension_level_id'] ?? '',
+      name: map['name'] ?? '',
+      description: map['description'] ?? '',
+      make: map['make'] ?? '',
+      model: map['model'] ?? '',
+      createdAt: map['created_at'] ?? '',
+      updatedAt: map['updated_at'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory EquipmentModel.fromJson(String source) =>
+      EquipmentModel.fromMap(json.decode(source));
+}
