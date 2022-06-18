6e10b74643924931db3fbde484c45f4e1416d9c2
Carlos Batista <carlos@strawti.com>
2022-06-18 12:51:35 -0300

    StepModel added

---
+++ lib/app/infra/models/step_model.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+class StepModel {
+  final int id;
+  final String name;
+  final String? description;
+  final int equipmentCategoryId;
+  final int projectId;
+
+  StepModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.equipmentCategoryId,
+    required this.projectId,
+  });
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'name': name});
+    result.addAll({'description': description});
+    result.addAll({'equipment_category_id': equipmentCategoryId});
+    result.addAll({'project_id': projectId});
+
+    return result;
+  }
+
+  factory StepModel.fromMap(Map<String, dynamic> map) {
+    return StepModel(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      description: map['description'] ?? '',
+      equipmentCategoryId: map['equipment_category_id']?.toInt() ?? 0,
+      projectId: map['project_id']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory StepModel.fromJson(String source) =>
+      StepModel.fromMap(json.decode(source));
+}
