915e49d4ad9cb5d739320f9ddfc3fcd035582b4c
Carlos Batista <carlos@strawti.com>
2022-06-18 13:42:32 -0300

    CompaniesProjectsStepsRepository added

---
+++ lib/app/infra/repositories/companies/projects/companies_projects_steps_repository.dart
@@ -0,0 +1,36 @@
+import 'package:fisplan_alupar/app/infra/api_endpoints.dart';
+import 'package:fisplan_alupar/app/infra/models/step_model.dart';
+import 'package:get/get.dart';
+
+import '../../../models/defaults/api_error_default_model.dart';
+import '../../../models/defaults/api_response_model.dart';
+import '../../../models/defaults/default_response_model.dart';
+
+class CompaniesProjectsStepsRepository {
+  final GetConnect _connect;
+  CompaniesProjectsStepsRepository(this._connect);
+
+  Future<ApiResponseModel<StepModel>> getAllByCompanyId(
+    int userCompanyId,
+  ) async {
+    final response = await _connect.get(
+      "$apiCompanies/$userCompanyId/by-projects/steps",
+    );
+
+    final responseModel = DefaultResponseModel.fromMap({
+      'statusCode': response.statusCode,
+      'data': response.body,
+    });
+
+    if (responseModel.success) {
+      return ApiResponseModel(
+        data: StepModel.fromMap(responseModel.data),
+      );
+    }
+
+    return ApiErrorDefaultModel(
+      message: 'Não foi possível obter dados',
+      response: responseModel,
+    );
+  }
+}
