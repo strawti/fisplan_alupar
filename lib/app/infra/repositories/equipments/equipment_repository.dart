7d45ae0f470f1e8a603273f7b5cadd8632970c8c
Carlos Batista <carlos@strawti.com>
2022-06-18 13:16:32 -0300

    EquipmentRepository added

---
+++ lib/app/infra/repositories/equipments/equipment_repository.dart
@@ -0,0 +1,32 @@
+import 'package:fisplan_alupar/app/infra/api_endpoints.dart';
+import 'package:get/get.dart';
+
+import '../../models/defaults/api_error_default_model.dart';
+import '../../models/defaults/api_response_model.dart';
+import '../../models/defaults/default_response_model.dart';
+import '../../models/equipment_model.dart';
+
+class EquipmentRepository {
+  final GetConnect _connect;
+  EquipmentRepository(this._connect);
+
+  Future<ApiResponseModel<EquipmentModel>> getAll() async {
+    final response = await _connect.get(apiEquipments);
+
+    final responseModel = DefaultResponseModel.fromMap({
+      'statusCode': response.statusCode,
+      'data': response.body,
+    });
+
+    if (responseModel.success) {
+      return ApiResponseModel(
+        data: EquipmentModel.fromMap(responseModel.data),
+      );
+    }
+
+    return ApiErrorDefaultModel(
+      message: 'Não foi possível obter dados',
+      response: responseModel,
+    );
+  }
+}
