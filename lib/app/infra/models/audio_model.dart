6b462c575fdf33c140f4709472a9b76c77d9b106
Carlos Batista <carlos@strawti.com>
2022-06-18 12:53:25 -0300

    AudioModel added

---
+++ lib/app/infra/models/audio_model.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+class AudioModel {
+  final String path;
+  AudioModel({
+    required this.path,
+  });
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+  
+    result.addAll({'path': path});
+  
+    return result;
+  }
+
+  factory AudioModel.fromMap(Map<String, dynamic> map) {
+    return AudioModel(
+      path: map['path'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AudioModel.fromJson(String source) => AudioModel.fromMap(json.decode(source));
+}
