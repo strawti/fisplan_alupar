d1f69a9d6d7b65a5d23852ef19c445e969990d78
Carlos Batista <carlos@strawti.com>
2022-06-18 12:53:10 -0300

    AnsewerModel added

---
+++ lib/app/infra/models/answer_model.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+class AnswerModel {
+  final int questionId;
+  final String answer;
+  
+  AnswerModel({
+    required this.questionId,
+    required this.answer,
+  });
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+  
+    result.addAll({'question_id': questionId});
+    result.addAll({'answer': answer});
+  
+    return result;
+  }
+
+  factory AnswerModel.fromMap(Map<String, dynamic> map) {
+    return AnswerModel(
+      questionId: map['question_id']?.toInt() ?? 0,
+      answer: map['answer'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AnswerModel.fromJson(String source) => AnswerModel.fromMap(json.decode(source));
+}
