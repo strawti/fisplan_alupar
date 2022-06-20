c36c99a3bf7ac6400fe6967d80943d011eda3f47
Carlos Batista <carlos@strawti.com>
2022-06-20 19:44:23 -0300

    Fix dependencies

--- lib/app/presenter/new_inspection/new_inspection_bindings.dart
+++ lib/app/presenter/new_inspection/new_inspection_bindings.dart
@@ -1,7 +1,12 @@
 import 'package:fisplan_alupar/app/infra/providers/installations/installations_type_provider.dart';
 import 'package:fisplan_alupar/app/infra/providers/local/installations/local_installations_type_provider.dart';
+import 'package:fisplan_alupar/app/infra/providers/local/local_towers_provider.dart';
+import 'package:fisplan_alupar/app/infra/providers/towers_provider.dart';
 import 'package:fisplan_alupar/app/infra/repositories/installations/installations_type_repository.dart';
 import 'package:fisplan_alupar/app/infra/repositories/local/installations/local_installations_type_repository.dart';
+import 'package:fisplan_alupar/app/infra/repositories/local/local_towers_repository.dart';
+import 'package:fisplan_alupar/app/infra/repositories/towers/towers_repository.dart';
+import 'package:fisplan_alupar/app/presenter/new_inspection/controllers/towers_controller.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -11,14 +16,22 @@ import 'controllers/installation_type_controller.dart';
 class NewInspectionBindings implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(() => InstallationsTypeRepository(Get.find()));
-
+    // Local
+    Get.lazyPut(() => LocalTowersRepository(GetStorage()));
     Get.lazyPut(() => LocalInstallationsTypeRepository(GetStorage()));
 
     Get.lazyPut(() => LocalInstallationsTypeProvider(Get.find()));
+    Get.lazyPut(() => LocalTowersProvider(Get.find()));
+
+    // External
+    Get.lazyPut(() => InstallationsTypeRepository(Get.find()));
+    Get.lazyPut(() => TowersRepository(Get.find()));
+
     Get.lazyPut(() => InstallationsTypeProvider(Get.find()));
+    Get.lazyPut(() => TowersProvider(Get.find()));
 
     Get.put(NewInspectionController());
     Get.put(InstallationTypeController(Get.find(), Get.find()));
+    Get.put(TowersController(Get.find(), Get.find()));
   }
 }
