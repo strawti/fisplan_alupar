3171f71fe81057ee99c1f843fd72b580a455c836
Carlos Batista <carlos@strawti.com>
2022-06-20 17:41:04 -0300

    LocalInstallationsRepository added

---
+++ lib/app/infra/repositories/local/installations/local_installations_repository.dart
@@ -0,0 +1,22 @@
+import 'package:fisplan_alupar/app/infra/api_endpoints.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../../models/installation_model.dart';
+
+class LocalInstallationsRepository {
+  final GetStorage _storage;
+  LocalInstallationsRepository(this._storage);
+
+  Future<void> setInstallations(List<InstallationModel> data) async {
+    await _storage.write(apiInstallations, data.map((e) => e.toJson()));
+  }
+
+  Future<List<InstallationModel>> getInstallations() async {
+    final data = await _storage.read(apiInstallations);
+    return data.map((e) => InstallationModel.fromJson(e)).toList();
+  }
+
+  Future<void> clearInstallations() async {
+    await _storage.remove(apiInstallations);
+  }
+}
