6329f9047926731d24e4e16bc84ef6b5979f9ddc
Carlos Batista <carlos@strawti.com>
2022-06-20 14:44:49 -0300

    Adjust the login response

--- lib/app/infra/repositories/auth/login_repository.dart
+++ lib/app/infra/repositories/auth/login_repository.dart
@@ -1,18 +1,16 @@
 import 'package:fisplan_alupar/app/infra/models/requests/auth/login_request_model.dart';
 import 'package:get/get.dart';
 
-import '../../../core/app_token.dart';
 import '../../api_endpoints.dart';
 import '../../models/defaults/api_error_default_model.dart';
 import '../../models/defaults/api_response_model.dart';
 import '../../models/defaults/default_response_model.dart';
-import '../../models/responses/user_response_model.dart';
 
 class LoginRepository {
   LoginRepository(this._connect);
   final GetConnect _connect;
 
-  Future<ApiResponseModel<UserResponseModel>> signIn(
+  Future<ApiResponseModel<String?>> signIn(
     LoginRequestModel requestModel,
   ) async {
     final response = await _connect.post(
@@ -29,11 +27,7 @@ class LoginRepository {
     });
 
     if (responseModel.success) {
-      await AppToken.instance.setToken(responseModel.data['token']);
-
-      return ApiResponseModel(
-        data: UserResponseModel.fromMap(responseModel.data),
-      );
+      return ApiResponseModel(data: responseModel.data['token']);
     }
 
     return ApiErrorDefaultModel(
