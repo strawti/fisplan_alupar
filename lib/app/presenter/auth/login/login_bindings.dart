21b671de92b43f17197a4504a65d23d4fb2cdbe6
Carlos Batista <carlos@strawti.com>
2022-06-20 09:50:24 -0300

    Dependencies for login

--- lib/app/presenter/auth/login/login_bindings.dart
+++ lib/app/presenter/auth/login/login_bindings.dart
@@ -1,9 +1,14 @@
+import 'package:fisplan_alupar/app/infra/providers/auth/login_provider.dart';
+import 'package:fisplan_alupar/app/infra/repositories/auth/login_repository.dart';
 import 'package:get/get.dart';
+
 import './login_controller.dart';
 
 class LoginBindings implements Bindings {
-    @override
-    void dependencies() {
-        Get.put(LoginController());
-    }
+  @override
+  void dependencies() {
+    Get.lazyPut(() => LoginRepository(Get.find()));
+    Get.lazyPut(() => LoginProvider(Get.find()));
+    Get.put(LoginController(Get.find()));
+  }
 }
