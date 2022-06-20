305f5233720a94f1e4795524760279a4bcb2021c
Carlos Batista <carlos@strawti.com>
2022-06-20 16:47:16 -0300

    GenericType in SelectionPageArguments

--- lib/app/routes/arguments/selection_page_arguments.dart
+++ lib/app/routes/arguments/selection_page_arguments.dart
@@ -1,8 +1,8 @@
 import 'package:fisplan_alupar/app/infra/models/defaults/item_selection_model.dart';
 
-class SelectionPageArguments {
+class SelectionPageArguments<T> {
   SelectionPageArguments({required this.title, required this.items});
 
   final String title;
-  final List<ItemSelectionModel> items;
+  final List<ItemSelectionModel<T>> items;
 }
