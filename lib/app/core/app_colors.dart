433a03c65bc3f77ca34afe2a4947f9a0dcaf44b9
Carlos Batista <carlos@strawti.com>
2022-06-20 09:10:16 -0300

    Colors added

--- lib/app/core/app_colors.dart
+++ lib/app/core/app_colors.dart
@@ -1,3 +1,70 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-const appColorPrimary = Color(0xFF00A8FF);
+const appPrimaryColor = Color(0xff003593);
+const appPrimaryColorCcontrast = Color(0xffffffff);
+const appPrimaryColorShade = Color(0xff002f81);
+const appPrimaryColorTint = Color(0xff1a499e);
+
+const appColorSecondary = Color(0xff809ac9);
+const appColorSecondaryContrast = Color(0xff000000);
+const appColorSecondaryShade = Color(0xff7188b1);
+const appColorSecondaryTint = Color(0xff8da4ce);
+
+const appColorTertiary = Color(0xff00a0e3);
+const appColorTertiaryContrast = Color(0xffffffff);
+const appColorTertiaryShade = Color(0xff008dc8);
+const appColorTertiaryTint = Color(0xff1aaae6);
+
+const appColorSuccess = Color(0xff10dc60);
+const appColorSuccessContrast = Color(0xffffffff);
+const appColorSuccessShade = Color(0xff0ec254);
+const appColorSuccessTint = Color(0xff28e070);
+
+const appColorWarning = Color(0xffffce00);
+const appColorWarningContrast = Color(0xffffffff);
+const appColorWarningShade = Color(0xffe0b500);
+const appColorWarningTint = Color(0xffffd31a);
+
+const appColorDanger = Color(0xfff04141);
+const appColorDangerContrast = Color(0xffffffff);
+const appColorDangerShade = Color(0xffd33939);
+const appColorDangerTint = Color(0xfff25454);
+
+const appColorDark = Color(0xff373435);
+const appColorDarkContrast = Color(0xffffffff);
+const appColorDarkShade = Color(0xff302e2f);
+const appColorDarkTint = Color(0xff4b4849);
+
+const appColorMedium = Color(0xff6f7273);
+const appColorMediumContrast = Color(0xffffffff);
+const appColorMediumShade = Color(0xff626465);
+const appColorMediumTint = Color(0xff7d8081);
+
+const appColorLight = Color(0xfff4f5f8);
+const appColorLightContrast = Color(0xff000000);
+const appColorLightShade = Color(0xffd7d8da);
+const appColorLightTint = Color(0xfff5f6f9);
+
+const appBackgrouncColor = Color(0xffffffff);
+
+const appTextColor = Color(0xff373435);
+
+const appColorStep50 = Color(0xfff5f5f5);
+const appColorStep100 = Color(0xffebebeb);
+const appColorStep150 = Color(0xffe1e1e1);
+const appColorStep200 = Color(0xffd7d6d7);
+const appColorStep250 = Color(0xffcdcccd);
+const appColorStep300 = Color(0xffc3c2c2);
+const appColorStep350 = Color(0xffb9b8b8);
+const appColorStep400 = Color(0xffafaeae);
+const appColorStep450 = Color(0xffa5a4a4);
+const appColorStep500 = Color(0xff9b9a9a);
+const appColorStep550 = Color(0xff918f90);
+const appColorStep600 = Color(0xff878586);
+const appColorStep650 = Color(0xff7d7b7c);
+const appColorStep700 = Color(0xff737172);
+const appColorStep750 = Color(0xff696768);
+const appColorStep800 = Color(0xff5f5d5d);
+const appColorStep850 = Color(0xff555253);
+const appColorStep900 = Color(0xff4b4849);
+const appColorStep950 = Color(0xff413e3f);
