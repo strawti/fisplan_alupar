9b2659c0c9a574d60f965960d67a05593451c957
Carlos Batista <carlos@strawti.com>
2022-06-20 16:45:33 -0300

    InstallationsTypeProvider added

---
+++ lib/app/infra/providers/installations/installations_type_provider.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:fisplan_alupar/app/infra/repositories/installations/installations_type_repository.dart';
+
+import '../../../core/app_constants.dart';
+import '../../models/defaults/app_error_model.dart';
+import '../../models/defaults/provider_response_model.dart';
+import '../../models/installation_type_model.dart';
+
+class InstallationsTypeProvider {
+  final InstallationsTypeRepository _repository;
+  InstallationsTypeProvider(this._repository);
+
+  Future<ProviderResponseModel<List<InstallationTypeModel>>> getAll() async {
+    try {
+      final response = await _repository.getAll();
+      return ProviderResponseModel.fromMap(response.toMap());
+    } on SocketException {
+      return AppErrorDefaultModel(constSocketExceptionError);
+    } catch (e) {
+      return AppErrorDefaultModel('InstallationsTypeProvider.getAll() $e');
+    }
+  }
+}
