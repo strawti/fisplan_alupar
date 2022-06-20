5dbef795961ae615066a39bb904ae5677facd8c0
Carlos Batista <carlos@strawti.com>
2022-06-20 15:21:31 -0300

    Icon Removed

--- lib/app/shared/widgets/button_default_widget.dart
+++ lib/app/shared/widgets/button_default_widget.dart
@@ -3,8 +3,6 @@ import 'package:fisplan_alupar/app/core/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../utils/ternary_clean.dart';
-
 class ButtonDefaultWidget extends StatelessWidget {
   final String title;
   final void Function() onTap;
@@ -15,7 +13,6 @@ class ButtonDefaultWidget extends StatelessWidget {
   final Color? backgroundColor;
   final bool isLoading;
   final bool enabled;
-  final Icon? icon;
 
   const ButtonDefaultWidget({
     Key? key,
@@ -28,61 +25,36 @@ class ButtonDefaultWidget extends StatelessWidget {
     this.backgroundColor,
     this.isLoading = false,
     this.enabled = true,
-    this.icon,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      margin: EdgeInsets.symmetric(
-        horizontal: marginHorizontal,
-      ),
+      margin: EdgeInsets.symmetric(horizontal: marginHorizontal),
       width: width,
       duration: const Duration(milliseconds: 500),
       child: MaterialButton(
-        color: backgroundColor ?? appColorSecondary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        padding: EdgeInsets.symmetric(
-          vertical: paddingVertical,
-        ),
+        color: backgroundColor ?? appPrimaryColor,
+        disabledColor: backgroundColor?.withOpacity(0.2) ??
+            appPrimaryColor.withOpacity(0.2),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        padding: EdgeInsets.symmetric(vertical: paddingVertical),
         onPressed: enabled && !isLoading ? onTap : null,
         child: Visibility(
           visible: !isLoading,
           replacement: const CircularProgressIndicator.adaptive(
             backgroundColor: Colors.white,
           ),
-          child: TernaryClean(
-            condition: icon != null,
-            caseTrue: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                icon ?? const SizedBox.shrink(),
-                AutoSizeText(
-                  title,
-                  style: Get.textTheme.headline1!.copyWith(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16.0,
-                    color: textColor,
-                  ),
-                  maxLines: 1,
-                  textAlign: TextAlign.center,
-                  minFontSize: 5,
-                ),
-              ],
+          child: AutoSizeText(
+            title,
+            style: Get.textTheme.headline1!.copyWith(
+              fontWeight: FontWeight.w500,
+              fontSize: 16.0,
+              color: textColor,
             ),
-            caseFalse: AutoSizeText(
-              title,
-              style: Get.textTheme.headline1!.copyWith(
-                fontWeight: FontWeight.w500,
-                fontSize: 16.0,
-                color: textColor,
-              ),
-              maxLines: 1,
-              textAlign: TextAlign.center,
-              minFontSize: 5,
-            ),
+            maxLines: 1,
+            textAlign: TextAlign.center,
+            minFontSize: 5,
           ),
         ),
       ),
