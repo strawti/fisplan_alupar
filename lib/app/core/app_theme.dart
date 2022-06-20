c086c28317a993e04c943ba4cf6c25754a97a7ed
Carlos Batista <carlos@strawti.com>
2022-06-20 09:16:31 -0300

    colors defined

--- lib/app/core/app_theme.dart
+++ lib/app/core/app_theme.dart
@@ -1,3 +1,7 @@
+import 'package:fisplan_alupar/app/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
-final appThemeLight = ThemeData();
+final appThemeLight = ThemeData(
+  primaryColor: appPrimaryColor,
+  splashColor: appPrimaryColor,
+);
