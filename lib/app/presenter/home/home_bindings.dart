a20bcbc64cf517f80d9d0519fba4312fc752d43a
Carlos Batista <carlos@strawti.com>
2022-06-20 14:46:17 -0300

    Dependencies added

--- lib/app/presenter/home/home_bindings.dart
+++ lib/app/presenter/home/home_bindings.dart
@@ -1,9 +1,20 @@
+import 'package:fisplan_alupar/app/infra/providers/auth/user_provider.dart';
+import 'package:fisplan_alupar/app/infra/repositories/auth/user_repository.dart';
+import 'package:fisplan_alupar/app/infra/repositories/local/local_user_repository.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
 import './home_controller.dart';
+import '../../infra/providers/local/local_user_provider.dart';
 
 class HomeBindings implements Bindings {
-    @override
-    void dependencies() {
-        Get.put(HomeController());
-    }
+  @override
+  void dependencies() {
+    Get.lazyPut(() => LocalUserRepository(GetStorage()));
+    Get.lazyPut(() => LocalUserProvider(Get.find()));
+    Get.lazyPut(() => UserRepository(Get.find()));
+    Get.lazyPut(() => UserProvider(Get.find()));
+
+    Get.put(HomeController());
+  }
 }
