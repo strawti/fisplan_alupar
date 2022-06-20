6173bd0741ce4940979543ca25d1b3db7b1245b5
Carlos Batista <carlos@strawti.com>
2022-06-20 09:28:06 -0300

    assets added

--- lib/app/core/app_assets.dart
+++ lib/app/core/app_assets.dart
@@ -1,4 +1,4 @@
-const appAssetLogo1024x500 = "assets/feature-1024x500.png";
-const appAssetIcon512 = "assets/icon-512.png";
+const appAssetLogo1024x500 = "assets/feature_1024x500.png";
+const appAssetIcon512 = "assets/icon_512.png";
 const appAssetSplash = "assets/splash.png";
-
+const appAssetLogo = 'assets/logo.png';
