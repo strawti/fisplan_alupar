6a982a9befe93a7366f30f5439613d38370f8952
Carlos Batista <carlos@strawti.com>
2022-06-20 17:56:34 -0300

    EquipmentsController added

---
+++ lib/app/presenter/new_inspection/controllers/equipments_controller.dart
@@ -0,0 +1,71 @@
+import 'package:fisplan_alupar/app/infra/models/equipment_model.dart';
+import 'package:fisplan_alupar/app/infra/providers/equipments/equipments_provider.dart';
+import 'package:get/get.dart';
+
+import '../../../core/app_connectivity.dart';
+import '../../../infra/providers/local/equipments/local_equipments_provider.dart';
+import '../../../shared/utils/custom_snackbar.dart';
+import '../../../shared/utils/loader_manager.dart';
+
+class EquipmentsController extends GetxController with LoaderManager {
+  final EquipmentsProvider _installationsProvider;
+  final LocalEquipmentsProvider _localEquipmentsProvider;
+
+  EquipmentsController(
+    this._installationsProvider,
+    this._localEquipmentsProvider,
+  );
+
+  @override
+  void onReady() {
+    super.onReady();
+    fetch();
+  }
+
+  List<EquipmentModel> equipments = [];
+
+  Future<void> fetch() async {
+    setIsLoading(true);
+
+    if (await AppConnectivity.instance.isConnected()) {
+      _getInstallations();
+    } else {
+      await _getLocalInstallations();
+    }
+
+    setIsLoading(false);
+  }
+
+  Future _getInstallations() async {
+    final response = await _installationsProvider.getAll();
+
+    if (response.isSuccess) {
+      equipments = response.data ?? [];
+      _setLocalInstallations(equipments);
+    } else {
+      CustomSnackbar.to.show(response.error!.content!);
+    }
+  }
+
+  Future _getLocalInstallations() async {
+    final response = await _localEquipmentsProvider.getAll();
+
+    if (response.isSuccess) {
+      equipments = response.data ?? [];
+    } else {
+      CustomSnackbar.to.show(response.error!.content!);
+    }
+  }
+
+  Future _setLocalInstallations(List<EquipmentModel> data) async {
+    final response = await _localEquipmentsProvider.setEquipments(
+      data,
+    );
+
+    if (response.isSuccess) {
+      equipments = data;
+    } else {
+      CustomSnackbar.to.show(response.error!.content!);
+    }
+  }
+}
