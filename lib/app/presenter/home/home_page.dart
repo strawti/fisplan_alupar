7addab7b8d7c5d46aca071b4d18a272c3a8bb199
Carlos Batista <carlos@strawti.com>
2022-06-20 17:29:31 -0300

    Test code removed

--- lib/app/presenter/home/home_page.dart
+++ lib/app/presenter/home/home_page.dart
@@ -1,11 +1,8 @@
-import 'package:fisplan_alupar/app/infra/models/defaults/item_selection_model.dart';
-import 'package:fisplan_alupar/app/presenter/new_inspection/new_inspection_bindings.dart';
-import 'package:fisplan_alupar/app/presenter/selection_page/selection_page.dart';
-import 'package:fisplan_alupar/app/routes/arguments/selection_page_arguments.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import './home_controller.dart';
+import '../new_inspection/new_inspection_page.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
@@ -23,27 +20,7 @@ class HomePage extends GetView<HomeController> {
             // TODO: DIALOG PARA CONFIRMAR SAIDA
             // PARA TESTE
 
-            NewInspectionBindings();
-
-            Get.toNamed(
-              SelectionPage.route,
-              arguments: SelectionPageArguments(
-                title: 'Tipo de Instalação',
-                items: [
-                  // Listas de itens relacionados com o contexto,
-                  ItemSelectionModel(
-                    item: '',
-                    isChecked: false,
-                    title: 'ABC da Amazônia',
-                  ),
-                  ItemSelectionModel(
-                    item: '',
-                    isChecked: true,
-                    title: 'Pato no Tucupi',
-                  ),
-                ],
-              ),
-            );
+            Get.toNamed(NewInspectionPage.route);
           },
         ),
       ),
