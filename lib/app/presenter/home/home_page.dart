63e3971dcab93e01c354685329f011e5ec079668
Carlos Batista <carlos@strawti.com>
2022-06-20 16:11:46 -0300

    test code

--- lib/app/presenter/home/home_page.dart
+++ lib/app/presenter/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:fisplan_alupar/app/infra/models/defaults/item_selection_model.dart';
+import 'package:fisplan_alupar/app/presenter/selection_page/selection_page.dart';
+import 'package:fisplan_alupar/app/routes/arguments/selection_page_arguments.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -17,6 +20,24 @@ class HomePage extends GetView<HomeController> {
           icon: const Text('Sair'),
           onPressed: () {
             // TODO: DIALOG PARA CONFIRMAR SAIDA
+            // PARA TESTE
+            Get.toNamed(
+              SelectionPage.route,
+              arguments: SelectionPageArguments(
+                title: 'Tipo de Instalação',
+                items: [
+                  // Listas de itens relacionados com o contexto,
+                  ItemSelectionModel(
+                    isChecked: false,
+                    title: 'ABC da Amazônia',
+                  ),
+                  ItemSelectionModel(
+                    isChecked: true,
+                    title: 'Pato no Tucupi',
+                  ),
+                ],
+              ),
+            );
           },
         ),
       ),
