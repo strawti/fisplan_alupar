faec6d2bbda6fa4f0f72484855a6579e5dfd6b03
Carlos Batista <carlos@strawti.com>
2022-06-20 16:45:26 -0300

    Generic Type added

--- lib/app/infra/models/defaults/item_selection_model.dart
+++ lib/app/infra/models/defaults/item_selection_model.dart
@@ -1,7 +1,10 @@
-class ItemSelectionModel {
+class ItemSelectionModel<T> {
+  final T item;
   final bool isChecked;
   final String title;
+  
   ItemSelectionModel({
+    required this.item,
     this.isChecked = false,
     required this.title,
   });
