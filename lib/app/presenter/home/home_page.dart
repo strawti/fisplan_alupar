70856f13d5440409fdd3e4d65b93b9a36b333379
Carlos Batista <carlos@strawti.com>
2022-06-20 09:51:24 -0300

    HomeModule added

---
+++ lib/app/presenter/home/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import './home_controller.dart';
+
+class HomePage extends GetView<HomeController> {
+  const HomePage({Key? key}) : super(key: key);
+
+  static const route = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: Container(),
+    );
+  }
+}
