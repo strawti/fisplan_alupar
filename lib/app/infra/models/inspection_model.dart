d21301ac7a42e4da7d112369ffac54175ed7c38d
Carlos Batista <carlos@strawti.com>
2022-06-22 09:42:19 -0300

    Moved to models/responses

--- lib/app/infra/models/inspection_model.dart
+++ lib/app/infra/models/inspection_model.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:fisplan_alupar/app/infra/models/responses/answer_model.dart';
-import 'package:fisplan_alupar/app/infra/models/audio_model.dart';
+import 'package:fisplan_alupar/app/infra/models/responses/audio_model.dart';
 import 'package:fisplan_alupar/app/infra/models/photo_model.dart';
 import 'package:flutter/material.dart';
 
