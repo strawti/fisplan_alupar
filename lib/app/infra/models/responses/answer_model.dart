63babea8016cf5e4374f7a54ec42f1a1dcf58762
Carlos Batista <carlos@strawti.com>
2022-06-28 21:03:43 -0300

    up: questionnaire_id required

--- lib/app/infra/models/responses/answer_model.dart
+++ lib/app/infra/models/responses/answer_model.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
 class AnswerModel {
+  final int questionnaireId;
   final int questionId;
 
   /// String | int
   final dynamic answer;
 
   AnswerModel({
+    required this.questionnaireId,
     required this.questionId,
     required this.answer,
   });
@@ -16,6 +18,7 @@ class AnswerModel {
 
     result.addAll({'question_id': questionId});
     result.addAll({'answer': answer});
+    result.addAll({'questionnaire_id': questionnaireId});
 
     return result;
   }
@@ -24,6 +27,7 @@ class AnswerModel {
     return AnswerModel(
       questionId: map['question_id']?.toInt() ?? 0,
       answer: "${map['answer']}",
+      questionnaireId: map['questionnaire_id']?.toInt() ?? 0,
     );
   }
 
