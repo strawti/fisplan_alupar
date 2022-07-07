68a8d042bc45a2b8b162b07ddc70fb901da52fc9
Carlos Batista <carloscoffeepy@gmail.com>
2022-07-07 16:22:30 -0300

    ref,fix: refactor and ajusts

--- lib/app/presenter/selection_page/selection_page.dart
+++ lib/app/presenter/selection_page/selection_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../shared/widgets/search_widget.dart';
 import 'selection_controller.dart';
+import 'widgets/body_selection_page.dart';
 
 class SelectionPage extends StatelessWidget {
   const SelectionPage({Key? key}) : super(key: key);
@@ -15,47 +15,7 @@ class SelectionPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(Get.find<SelectionController>().routeArguments.title),
       ),
-      body: Column(
-        children: [
-          SearchWidget<SelectionController>(
-            controller: Get.find<SelectionController>().searchController,
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: SingleChildScrollView(
-              child: GetBuilder<SelectionController>(
-                builder: (control) {
-                  if (control.itemsFiltered.isEmpty) {
-                    return const Center(
-                      child: Text('Nenhum item encontrado'),
-                    );
-                  }
-
-                  return Column(
-                    children: List.generate(
-                      control.itemsFiltered.length,
-                      (index) {
-                        final item = control.itemsFiltered[index];
-                        return ListTile(
-                          title: Text(item.title),
-                          onTap: () => Get.back(result: item),
-                          leading: Checkbox(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            value: item.isChecked,
-                            onChanged: (value) => Get.back(result: item),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: const BodySelectionPage(),
     );
   }
 }
