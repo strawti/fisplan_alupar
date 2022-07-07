214da635c1be442a23f1fd9f814872c9195c5270
Carlos Batista <carloscoffeepy@gmail.com>
2022-07-07 16:13:19 -0300

    ref: renamed formatDateTimeForString

--- lib/app/shared/utils/get_datetime.dart
+++ lib/app/shared/utils/get_datetime.dart
@@ -1,4 +1,4 @@
-String getDateTime(DateTime? dateTime) {
+String formatDateTimeForString(DateTime? dateTime) {
   if (dateTime == null) {
     return 'Não informado';
   }
@@ -10,4 +10,19 @@ String getDateTime(DateTime? dateTime) {
   final minute = dateTime.minute.toString().padLeft(2, '0');
   final second = dateTime.second.toString().padLeft(2, '0');
   return "$day/$month/$year $hour:$minute:$second";
+}
+
+DateTime formatStringForDateTime(String value) {
+  final valueAsList =
+      value.replaceAll(':', '-').replaceAll(' ', '-').split('-');
+
+  int year = int.parse(valueAsList[0]);
+  int month = int.parse(valueAsList[1]);
+  int day = int.parse(valueAsList[2]);
+
+  int hours = int.parse(valueAsList[2]);
+  int minutes = int.parse(valueAsList[3]);
+  int seconds = int.parse(valueAsList[4]);
+
+  return DateTime(year, month, day, hours, minutes, seconds);
 }
