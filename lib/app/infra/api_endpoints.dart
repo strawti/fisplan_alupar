c20e7633fb131c4725ba800bfce23766a1dc32d2
Carlos Batista <carloscoffeepy@gmail.com>
2022-07-07 16:18:50 -0300

    ref: removed unnecessary code

--- lib/app/infra/api_endpoints.dart
+++ lib/app/infra/api_endpoints.dart
@@ -1,9 +1,6 @@
 // PRODUCTION
 const apiBase = 'https://fisplan.alupar.com.br/api';
 
-// DEVELOPMENT
-//const apiBase = '';
-
 /// Endpoints
 const apiLogin = '$apiBase/auth/login';
 const apiMe = '$apiBase/auth/me';
@@ -16,8 +13,6 @@ const apiActivities = '$apiBase/activities';
 const apiEquipmentCategories = '$apiBase/equipment-categories';
 const apiEquipments = '$apiBase/equipments';
 const apiInspections = '$apiBase/inspections';
-// for pictures
-// inspections/${upload.server_inspection_id}/photo
 
 const apiInspectionsSyncSingle = '$apiBase/inspections/sync-single';
 
@@ -25,10 +20,5 @@ const apiInstallationTypes = '$apiBase/installation-types';
 const apiInstallations = '$apiBase/installations';
 
 const apiCompanies = '$apiBase/companies';
-// for projects
-// companies/${user.get('company_id')}/projects
-// companies/${user.get('company_id')}/by-projects/questionnaires
-// companies/${user.get('company_id')}/by-projects/steps
-// companies/${user.get('company_id')}/tension-levels
 
 const apiTowers = '$apiBase/towers';
